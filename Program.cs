03e81906427cdd5c549949865304c049012900a2
Vincent Xu <vincent.xu1@outlook.com>
2025-08-05 13:33:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_5._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RecurseNaturalNumbers(10);
+            Console.WriteLine();
+        }
+
+        static void RecurseNaturalNumbers(int n)
+        {
+            if (n > 1)
+                RecurseNaturalNumbers(n - 1);
+            Console.Write(n + " ");
+        }
+    }
+}
